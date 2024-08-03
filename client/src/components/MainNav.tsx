88cc7f0be1ffabaa90f3c15a95010ef46d03e2ab
Akylbek04 <zamirovakylbek920@gmail.com>
2024-08-03 17:51:07 +0600

    created main-navbar

---
+++ client/src/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold hover:text-lime-500 hover:bg-white"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default MainNav;
