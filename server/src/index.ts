8026d9b16c8dd4ec330426e6ceab133cc867af2a
Akylbek04 <zamirovakylbek920@gmail.com>
2024-08-04 18:30:25 +0600

    added user-model-api --fixed

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import MyUserRoute from "./routes/MyUserRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/my/user", MyUserRoute);
 
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello!" });
