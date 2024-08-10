3d181c1491ce05fccf88bc086224f694c7ab8f98
Akylbek Zamirov <124916251+akylbek04@users.noreply.github.com>
2024-08-10 19:27:40 +0600

    added fn to update user profile

--- client/src/api/MyUserApi.tsx
+++ client/src/api/MyUserApi.tsx
@@ -14,22 +14,18 @@ export const useCreateMyUser = () => {
   const createMyUserRequest = async (user: CreateUserRequest) => {
     try {
       const accessToken = await getAccessTokenSilently();
-      const response = await fetch(
-        `${API_BASE_URL}/api/my/user`,
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(user),
-        }
-      );
+      const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
 
       if (!response.ok) throw new Error("failed to create a user");
 
       return response.json();
-
     } catch (error) {
       console.log(error);
     }
@@ -47,5 +43,54 @@ export const useCreateMyUser = () => {
     isError,
     isLoading,
     isSuccess,
+  };
+};
+
+type UpdateMyUserRequest = {
+  name: string;
+  addressLine1: string;
+  city: string;
+  country: string;
+};
+
+export const useUpdateMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateMyUserRequest = async (formData: UpdateMyUserRequest) => {
+    try {
+      const accessToken = await getAccessTokenSilently();
+      const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) throw new Error("failed to update a user");
+
+      return response.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const {
+    mutateAsync: updateUser,
+    isError,
+    isLoading,
+    isSuccess,
+    error,
+    reset,
+  } = useMutation(updateMyUserRequest);
+
+  return {
+    updateUser,
+    isError,
+    isLoading,
+    isSuccess,
+    error,
+    reset,
   };
 };
