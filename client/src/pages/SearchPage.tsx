25e88d69bae48b79f211e677be2c9ba600705639
Akylbek04 <zamirovakylbek920@gmail.com>
2024-08-26 19:18:00 +0600

    added serch API request

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,9 +1,23 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const { city } = useParams();
+  const { results } = useSearchRestaurants(city);
 
-  return <div> User searched for {city}</div>;
+  return (
+    <div>
+      {" "}
+      User searched for {city}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </div>
+  );
 };
 
 export default SearchPage;
