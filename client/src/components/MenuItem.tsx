02c3eed8cc4b92b245d46e48e70988fd272890ca
Akylbek04 <zamirovakylbek920@gmail.com>
2024-08-29 14:52:45 +0600

    added detail page and menu items

---
+++ client/src/components/MenuItem.tsx
@@ -0,0 +1,21 @@
+import { MenuItem as MenuItemType } from "@/types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+type Props = {
+  menuItem: MenuItemType;
+};
+
+const MenuItem = ({ menuItem }: Props) => {
+  return (
+    <Card className="cursor-pointer ">
+      <CardHeader>
+        <CardTitle>{menuItem.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="font-bold ">
+        £{(menuItem.price / 100).toFixed(2)}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MenuItem;
